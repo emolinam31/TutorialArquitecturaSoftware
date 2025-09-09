b9f49b7cce915316dc2026bf64bd2c4d6e648916
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-09-09 17:50:31 -0500

    Literal B Terminado

--- todoapp/backend/api/views.py
+++ todoapp/backend/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import ToDoSerializer
 from todo.models import ToDo
 
@@ -7,7 +7,7 @@ class TodoListCreate(generics.ListCreateAPIView):
     # queryset.
     # We specify ToDoSerializer which we have earlier implemented
     serializer_class = ToDoSerializer
-    
+    permission_classes = [permissions.IsAuthenticated]
     def get_queryset(self):
         user = self.request.user
         return ToDo.objects.filter(user=user).order_by('-created')
